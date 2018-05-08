b98d23b0fe90ee4fe3a868c0c26e6aeae501a4ce
Oliver Grey <2293560686@qq.com>
2018-05-08 10:54:29 +0800

    commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # webpack-demo
 1.yarn add webpack webpack-cli --dev
 2.yarn add html-webpack-plugin --dev
+------------------------------------
+3.yarn add webpack-dev-server --dev
+4.yarn add nodemon --dev (optional)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devServer: {
+        stats: "errors-only",
+        host: process.env.HOST,
+        port: process.env.PORT,
+        open: true,
+        overlay: true
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack demo'
