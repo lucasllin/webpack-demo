a1ae09ec904ad0b6b6121825034733ba55eec420
Oliver Grey <2293560686@qq.com>
2018-05-08 11:08:36 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
+const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    devServer: {
-        stats: "errors-only",
+const parts = require('./webpack.parts');
+
+const commonConfig = merge([
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Webpack demo'
+            })
+        ]
+    }
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
         host: process.env.HOST,
         port: process.env.PORT,
-        open: true,
-        overlay: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack demo'
-        })
-    ]
+    }),
+]);
+
+module.exports = mode => {
+    if (mode === "production") {
+        return merge(commonConfig, productionConfig, {mode});
+    }
+
+    return merge(commonConfig, developmentConfig, {mode});
 };
